fc55c3ef26266d45c226a31fa3ef4fe3386a283f
oscarfhdev <oscarfh.dev@gmail.com>
2025-10-17 21:11:23 +0200

    UPDATED TEMA 1/ACTIVIDAD 2--> finished task2

---
+++ TEMA 1/ACTIVIDAD 2/js/script2.js
@@ -0,0 +1,9 @@
+function enfocar(input){
+    input.style.backgroundColor = "lightgray";
+    input.style.color = "blue";
+}
+
+function desenfocar(input){
+    input.style.backgroundColor = "";
+    input.style.color = "";
+}
