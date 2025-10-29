87a3aa94899388e762191471cde07ae180d7b57d
oscarfhdev <oscarfh.dev@gmail.com>
2025-10-29 11:04:03 +0100

    UPDATED TEMA 1/Funciones del Tiempo/Ejercicio5 --> task 5 finished

--- TEMA 1/Funciones del Tiempo/js/script5.js
+++ TEMA 1/Funciones del Tiempo/js/script5.js
@@ -1,12 +1,14 @@
 let contadorSegundos = 0;
 let contadorMinutos = 0;
 
+// Obtenemos el gif
+const gif = document.getElementById('gif1');
+
 
 // Esta función solo se llama si han pasado 5 segundos
 function ocultarDivYGenerearTabla(){
-    // Obtenemos los elementos como el div principal y el gif
+    // Obtenemos los elementos como el div principal
     const divPrincipal = document.getElementById('principal');
-    const gif = document.getElementById('gif1');
 
     const tabla = document.createElement('table'); // Creamos un elemento tabla
 
@@ -39,9 +41,21 @@ function sumarSegundos(){ // Se llama automáticamente cada segundo
 
     cadena.textContent = contadorMinutos + " minutos y " + contadorSegundos++ + " segundos en esta página"; // Editamos el contenido del texto añadiendo los minutos
 
-
     // Aquí añadimos la condición de que en el caso de que llegue a los 5 segundos llame al método
     if (contadorSegundos === 6 && contadorMinutos === 0){
         ocultarDivYGenerearTabla();
     }
+
+    desplazarGif();
+}
+
+
+// Esta función se llama al iniciar el html, ya que la llama la función sumarSegundos()
+let posicion = 0;
+function desplazarGif(){
+    setInterval(() => { // Hacemos que esto se ejecute cada 1ms por la función arrow
+        if(contadorSegundos <= 6 ) // Verificamos que pare
+        posicion += 10; // Incrementamos el valor
+        gif.style.marginLeft = posicion + "px"; // Ahora se lo asignamos con px
+  }, 100);    
 }
